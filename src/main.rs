4cda5116aa24ff8eaa8544a09e0bbc94041c2054
ilyassjaatit <ilyassjaatit@gmail.com>
2022-10-01 00:27:16 +0200

    first steps in std::io

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
-fn main() {
-    // variables
-    let string_perfect_number: &str = "This is first perfect namber:";
-    let first_perfect_number: u16 = 6;
+use std::io;
 
-    // formating text
-    println!("{} {}", string_perfect_number, first_perfect_number);
+fn main() {
+    
+
+    // Declarar variable mutable string
+    let mut name = String::new();
+    let mut age : String = String::new();
+
+    println!("What is your name?");
+    // set name by terminal 
+    io::stdin().read_line(&mut name).unwrap();
+    println!("How are old you?");
+    // set age 
+    io::stdin().read_line(&mut age).unwrap();
+
+    let age_int :u8 = age.trim().parse().unwrap();
+
+    println!("Hi! My name is {} and I am {} years old.", name, age_int)
+
 }
